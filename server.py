84cf2330d2151b4bf6914fc2e57706596136be06
deekshi13 <120713734+deekshi13@users.noreply.github.com>
2023-01-10 21:29:27 +0530

    Delete server.py

--- server.py
+++
@@ -1,9 +0,0 @@
-import socket
-sk=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-sk.bind((socket.gethostname(),1546))
-sk.listen()
-while True:
-    client,address=sk.accept()
-    print("connection established")
-    print(address)
-    client.send(b"socket programming")
